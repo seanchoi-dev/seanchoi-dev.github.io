216ae0c8d313182c630d847f40d85faf20bfa8ed
Sean Choi <seanchoi@adobe.com>
2022-05-23 19:02:50 -0600

    no message

--- menu/scripts.js
+++
@@ -1,9 +0,0 @@
-const KEY = 'AIzaSyAR92f5jYyCpeME_-k2venYrHXEbSDb7MY';
-
-const init = async () => {
-  const res = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/1SaUGu_DNEL6qHEAn_MkYMf62zKpq5RwHFYmJavIlcxg/values/Sheet1?key=${KEY}`);
-  const data = await res.json();
-  console.log(data.values);
-}
-
-init();
