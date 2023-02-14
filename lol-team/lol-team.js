971a5965cec1acb8969be2d1fcfc1c99476ac377
Sean <newnesid@gmail.com>
2023-02-14 04:06:18 -0700

    be wip

--- lol-team/lol-team.js
+++ lol-team/lol-team.js
@@ -3,51 +3,51 @@ const getNewParticipant = (index) => {
 <div id="mix_players__${index}" class="participant-div participant-div-form row">
     <div class="col-md-4">
         <div class="input-group">
-            <input type="text" id="mix_players_${index}_name" name="mix[players][${index}][name]" class="form-control input-participants" placeholder="Participant ${index+1}" value="${index}" control-id="ControlID-5">
+            <input type="text" id="mix_players_${index}_name" name="mix.players.${index}.name" class="form-control input-participants" placeholder="Participant ${index+1}" value="${index}" control-id="ControlID-5">
         </div>
     </div>
     <div class="col-md-3 positions">
         <div id="mix_players_${index}_position" class="d-flex align-items-end justify-content-center">
             <label for="position_all_${index}" id="label_position_all_${index}" class="mx-1 label-position label-position-all active"></label>
-            <input name="mix[players][${index}][position][all]" type="checkbox" class="position-item" data-index="${index}" data-position="all" id="position_all_${index}" checked>
+            <input name="mix.players.${index}.position.all" type="checkbox" class="position-item" data-index="${index}" data-position="all" id="position_all_${index}" checked>
             <label for="position_top_${index}" id="label_position_top_${index}" class="mx-1 label-position label-position-top"></label>
-            <input name="mix[players][${index}][position][top]" type="checkbox" class="position-item" data-index="${index}" data-position="top" id="position_top_${index}">
+            <input name="mix.players.${index}.position.top" type="checkbox" class="position-item" data-index="${index}" data-position="top" id="position_top_${index}">
             <label for="position_jungle_${index}" id="label_position_jungle_${index}" class="mx-1 label-position label-position-jungle"></label>
-            <input name="mix[players][${index}][position][jungle]" type="checkbox" class="position-item" data-index="${index}" data-position="jungle" id="position_jungle_${index}">
+            <input name="mix.players.${index}.position.jungle" type="checkbox" class="position-item" data-index="${index}" data-position="jungle" id="position_jungle_${index}">
             <label for="position_mid_${index}" id="label_position_mid_${index}" class="mx-1 label-position label-position-mid"></label>
-            <input name="mix[players][${index}][position][mid]" type="checkbox" class="position-item" data-index="${index}" data-position="mid" id="position_mid_${index}">
+            <input name="mix.players.${index}.position.mid" type="checkbox" class="position-item" data-index="${index}" data-position="mid" id="position_mid_${index}">
             <label for="position_adc_${index}" id="label_position_adc_${index}" class="mx-1 label-position label-position-adc"></label>
-            <input name="mix[players][${index}][position][adc]" type="checkbox" class="position-item" data-index="${index}" data-position="adc" id="position_adc_${index}">
+            <input name="mix.players.${index}.position.adc" type="checkbox" class="position-item" data-index="${index}" data-position="adc" id="position_adc_${index}">
             <label for="position_support_${index}" id="label_position_support_${index}" class="mx-1 label-position label-position-support"></label>
-            <input name="mix[players][${index}][position][support]" type="checkbox" class="position-item" data-index="${index}" data-position="support" id="position_support_${index}">
+            <input name="mix.players.${index}.position.support" type="checkbox" class="position-item" data-index="${index}" data-position="support" id="position_support_${index}">
         </div>
     </div>
     <div class="col-md-5 no-padding-left">
         <div id="mix_players_${index}_level" class="level-participant d-block">
             <label for="mix_players_${index}_level_1" class="required">B</label>
-            <input type="radio" id="mix_players_${index}_level_1" name="mix[players][${index}][level]" required="required" value="1">
+            <input type="radio" id="mix_players_${index}_level_1" name="mix.players.${index}.level" required="required" value="1">
             <label for="mix_players_${index}_level_2" class="required">BS</label>
-            <input type="radio" id="mix_players_${index}_level_2" name="mix[players][${index}][level]" required="required" value="2">
+            <input type="radio" id="mix_players_${index}_level_2" name="mix.players.${index}.level" required="required" value="2">
             <label for="mix_players_${index}_level_3" class="required">S</label>
-            <input type="radio" id="mix_players_${index}_level_3" name="mix[players][${index}][level]" required="required" value="3">
+            <input type="radio" id="mix_players_${index}_level_3" name="mix.players.${index}.level" required="required" value="3">
             <label for="mix_players_${index}_level_4" class="required">SG</label>
-            <input type="radio" id="mix_players_${index}_level_4" name="mix[players][${index}][level]" required="required" value="4">
+            <input type="radio" id="mix_players_${index}_level_4" name="mix.players.${index}.level" required="required" value="4">
             <label for="mix_players_${index}_level_5" class="required">G</label>
-            <input type="radio" id="mix_players_${index}_level_5" name="mix[players][${index}][level]" required="required" value="5">
+            <input type="radio" id="mix_players_${index}_level_5" name="mix.players.${index}.level" required="required" value="5">
             <label for="mix_players_${index}_level_6" class="required">GP</label>
-            <input type="radio" id="mix_players_${index}_level_6" name="mix[players][${index}][level]" required="required" value="6">
+            <input type="radio" id="mix_players_${index}_level_6" name="mix.players.${index}.level" required="required" value="6">
             <label for="mix_players_${index}_level_7" class="required">P</label>
-            <input type="radio" id="mix_players_${index}_level_7" name="mix[players][${index}][level]" required="required" value="7">
+            <input type="radio" id="mix_players_${index}_level_7" name="mix.players.${index}.level" required="required" value="7">
             <label for="mix_players_${index}_level_8" class="required">PD</label>
-            <input type="radio" id="mix_players_${index}_level_8" name="mix[players][${index}][level]" required="required" value="8">
+            <input type="radio" id="mix_players_${index}_level_8" name="mix.players.${index}.level" required="required" value="8">
             <label for="mix_players_${index}_level_9" class="required">D</label>
-            <input type="radio" id="mix_players_${index}_level_9" name="mix[players][${index}][level]" required="required" value="9">
+            <input type="radio" id="mix_players_${index}_level_9" name="mix.players.${index}.level" required="required" value="9">
             <label for="mix_players_${index}_level_10" class="required">DM</label>
-            <input type="radio" id="mix_players_${index}_level_10" name="mix[players][${index}][level]" required="required" value="10">
+            <input type="radio" id="mix_players_${index}_level_10" name="mix.players.${index}.level" required="required" value="10">
             <label for="mix_players_${index}_level_11" class="required">M</label>
-            <input type="radio" id="mix_players_${index}_level_11" name="mix[players][${index}][level]" required="required" value="11">
+            <input type="radio" id="mix_players_${index}_level_11" name="mix.players.${index}.level" required="required" value="11">
             <label for="mix_players_${index}_level_12" class="required">H</label>
-            <input type="radio" id="mix_players_${index}_level_12" name="mix[players][${index}][level]" required="required" value="12">
+            <input type="radio" id="mix_players_${index}_level_12" name="mix.players.${index}.level" required="required" value="12">
         </div>
     </div>
 </div>`;
--- lol-team/match-result.js
+++ lol-team/match-result.js
@@ -1,3 +1,23 @@
 const { search } = window.location;
 const participantObj = JSON.parse('{"' + decodeURI(search.substring(1)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
 console.log(participantObj);
+const mixs = {};
+mixs.player = {};
+let playerIndex = 0;
+for (const [key, value] of Object.entries(participantObj)) {
+    if (!key.includes('players'));
+    const mix = key.split('.');
+    if (mix[2] !== playerIndex) {
+        playerIndex = mix[2];
+        mixs.player[playerIndex] = {};
+    }
+
+    if ('position' === mix[3]) {
+        mixs.player[playerIndex].position ??= {};
+        mixs.player[playerIndex].position[mix[4]] = value;
+    }
+    else {
+        mixs.player[playerIndex][mix[3]] = value;
+    }    
+}
+console.log(mixs.player);
