812faa09f8c0fbaaa6be58669b4c390bdb5ed1eb
Sean <newnesid@gmail.com>
2023-02-15 02:00:03 -0700

    trying to figure out the algolithm

--- lol-team/match-result.js
+++ lol-team/match-result.js
@@ -1,10 +1,10 @@
 const { search } = window.location;
 const participantObj = JSON.parse('{"' + decodeURI(search.substring(1)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
-console.log(participantObj);
-const mixs = {};
-mixs.player = {};
+const players = [];
+
 let playerIndex = 0;
 let balancedBy = '';
+console.log(participantObj)
 for (const [key, value] of Object.entries(participantObj)) {
     if (!key.includes('players')) {
         balancedBy = value;
@@ -13,36 +13,67 @@ for (const [key, value] of Object.entries(participantObj)) {
     const mix = key.split('.');
     if (mix[2] !== playerIndex) {
         playerIndex = mix[2];
-        mixs.player[playerIndex] = {};
+        players[playerIndex] = {};
     }
 
     if ('position' === mix[3]) {
-        mixs.player[playerIndex].position ??= {};
-        mixs.player[playerIndex].position[mix[4]] = value;
+        players[playerIndex].position ??= {};
+        players[playerIndex].position[mix[4]] = value;
     }
     else {
-        mixs.player[playerIndex][mix[3]] = value;
+        players[playerIndex][mix[3]] = value;
     }
+    players[playerIndex].index = playerIndex;
 }
+players.sort((a, b)=>{
+    return parseInt(b.level) - parseInt(a.level);
+ });
 
-const balancedByLevel = (players) => {
-    arr.sort(mixs.player).reverse()
-    let a = [], b = [], sumA = 0, sumB = 0, i = 0
-
-    while (i < arr.length) {
-        if (!sumA && !sumB || sumA == sumB) {
-            a.push(arr[i])
-            sumA += arr[i]
-        } else if (sumA < sumB) {
-            a.push(arr[i])
-            sumA += arr[i];
-        } else if (sumB < sumA) {
-            b.push(arr[i])
-            sumB += arr[i];
+const balancedByLevel = (n) => {
+    const asm = players => players.reduce((a,c)=>a + parseInt(c.level), 0);
+    const asm2 = arr=>arr.reduce((a,c)=>a+c,0);
+    let lim = Math.ceil(asm(players)/n),
+        loopend = players.length*n;
+        dist=[...Array(n)].map(()=>[]);
+    for (let i = 0; players.length && i < loopend; i++) {
+        if (parseInt(asm2(dist[i%n])) + parseInt(players[0].level)<=lim) {
+            dist[i % n].push(players.shift());
+           
         }
-        i++
     }
-    console.log(`Total: ${sumA} ${sumB}`)
-    return [a, b]
+    // console.log("lim:",lim);
+    if (players.length) console.log(lim,"Not all elements were distributed!")
+    return dist
 }
-console.log(mixs.player, balancedBy);
+
+
+const result = balancedByLevel(2);
+console.log(result);
+console.log("sums:", result.map(a=>a.reduce((s,c)=>s + parseInt(c.level), 0)));
+
+
+console.log(balancedBy);
+
+var sk=[9,7,6,5,4,8,7,5,4,2],n=2;
+function greedy(arr,n){
+  const asm=arr=>arr.reduce((a,c)=>a+c,0);
+  console.log('asm',asm(arr));
+  let lim=Math.ceil(asm(arr)/n),
+      loopend=sk.length*n;
+      srt=arr.slice(0).sort((a,b)=>b-a),
+      dist=[...Array(n)].map(()=>[]);
+      console.log('srt', srt);
+  for (let i=0; srt.length && i<loopend ; i++) {
+    if (asm(dist[i%n])+srt[0]<=lim) {
+        dist[i%n].push(srt.shift());
+    }
+  }
+  console.log("lim:",lim);
+  if (srt.length) console.log(lim,"Not all elements were distributed!")
+  return dist
+}
+
+var res=greedy(sk,n);
+
+console.log(res);
+console.log("sums:",res.map(a=>a.reduce((s,c)=>s+c)));
