684781c3f5abc4d61675121e2a7684e1eb383ae4
Sean <newnesid@gmail.com>
2023-02-16 04:57:20 -0700

    title added for result page

--- lol-team/match-result.js
+++ lol-team/match-result.js
@@ -12,6 +12,9 @@ const levelMap = {
     11: 'M',
     12: 'H'
 };
+const positionOrder = {
+    
+}
 const { search } = window.location;
 const participantObj = JSON.parse('{"' + decodeURI(search.substring(1)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
 let players = [];
